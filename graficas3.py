0299314d77add367b87adf4ed77e4c5f2415b93d
Sebastian Rojas <jhsrojasro@unal.edu.co>
2021-12-13 21:26:22 -0500

    Speed Up

--- graficas3.py
+++ graficas3.py
@@ -11,6 +11,7 @@ with open('reporte_3.txt') as input:
         data.append(re.findall("[0-9]+.[0-9]*", line))
 datos = np.matrix(data)
 
+#Response Time
 for i in range(0,len(data),6):
     X = [ '('+bloques.strip() +','+ hilos.strip()+')' for _,bloques, hilos,_ in data[i:i+6] ]
     Y = np.squeeze(np.asarray(datos[i : i + 6, 3].astype(float)))
@@ -20,30 +21,19 @@ for i in range(0,len(data),6):
     plt.xlabel("Número de bloques, Número de hilos por bloque")
     plt.ylabel("Response Time (seg)")
     plt.savefig('graficas/CUDA Response Time'+data[i][0]+'.jpg')
-"""
-#Response Time
-for kernel in range(3):
-    plt.clf()
-    plt.plot(x,datos[kernel*15 : kernel*15 + 5])
-    plt.plot(x,datos[kernel*15 + 5 : kernel*15 + 10])
-    plt.plot(x,datos[kernel*15 + 10 : kernel*15 + 15])
-    plt.legend(['720p','1080p','4k'])
-    plt.title("OMP Response Time Kernel "+str(kernel+1))
-    plt.xlabel("Número de hilos")
-    plt.ylabel("Response Time (seg)")
-    plt.savefig('graficas/OMP Response Time Kernel '+str(kernel+1)+'.jpg')
 
 #Speed Up
-for kernel in range(3):
+secuencial = [0.882095, #720p
+            2.113801, #1080p
+            8.847241, #4k
+            30.082776, #8k
+            71.120695] #12k
+for i in range(0,len(data),6):
+    X = [ '('+bloques.strip() +','+ hilos.strip()+')' for _,bloques, hilos,_ in data[i:i+6] ]
+    Y = np.squeeze(np.asarray(datos[i : i + 6, 3].astype(float)))
     plt.clf()
-    plt.plot(x,datos[kernel*15] / datos[kernel*15 : kernel*15 + 5])
-    plt.plot(x,datos[kernel*15 + 5 ] / datos[kernel*15 + 5 : kernel*15 + 10])
-    plt.plot(x,datos[kernel*15 + 10] / datos[kernel*15 + 10 : kernel*15 + 15])
-    plt.legend(['720p','1080p','4k'])
-    plt.title("SpeedUp Kernel "+str(kernel+1))
-    plt.xlabel("Número de hilos")
-    plt.ylabel("SpeedUp")
-    plt.savefig('graficas/Speed Up Kernel '+str(kernel+1)+'.jpg')
-
-
-"""
+    plt.plot( X, (secuencial[i//6]) / Y)
+    plt.title("CUDA Speed Up "+data[i][0])
+    plt.xlabel("Número de bloques, Número de hilos por bloque")
+    plt.ylabel("Speed Up")
+    plt.savefig('graficas/CUDA Speed Up '+data[i][0]+'.jpg')
