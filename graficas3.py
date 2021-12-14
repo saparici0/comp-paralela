7da3d9bb0b6281b509209a74e41824297f8b47f3
Sebastian Rojas <jhsrojasro@unal.edu.co>
2021-12-13 19:32:40 -0500

    python script

--- graficas3.py
+++ graficas3.py
@@ -1,11 +1,26 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import re
 from scipy.interpolate import make_interp_spline
 
 
-datos = np.genfromtxt('reporte_3.txt',delimiter=':')[:,1]
-x = ['1','2','4','8','16']
+#datos = np.genfromtxt('reporte_3.txt', delimiter=',')[:,2]
+data = []
+with open('reporte_3.txt') as input:
+    for line in input:
+        data.append(re.findall("[0-9]+.[0-9]*", line))
+datos = np.matrix(data)
 
+for i in range(0,len(data),6):
+    X = [ '('+bloques.strip() +','+ hilos.strip()+')' for _,bloques, hilos,_ in data[i:i+6] ]
+    Y = np.squeeze(np.asarray(datos[i : i + 6, 3].astype(float)))
+    plt.clf()
+    plt.plot( X, Y)
+    plt.title("CUDA Response Time "+data[i][0])
+    plt.xlabel("Número de bloques, Número de hilos por bloque")
+    plt.ylabel("Response Time (seg)")
+    plt.savefig('graficas/CUDA Response Time'+data[i][0]+'.jpg')
+"""
 #Response Time
 for kernel in range(3):
     plt.clf()
@@ -31,3 +46,4 @@ for kernel in range(3):
     plt.savefig('graficas/Speed Up Kernel '+str(kernel+1)+'.jpg')
 
 
+"""
